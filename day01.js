f6f797b320b6cbe177e2b2380d5a1e7e7ca25bfc
Vishnu Navghare <vishnunavg@gmail.com>
2026-06-20 10:36:42 +0530

    js

---
+++ day01.js
@@ -0,0 +1,34 @@
+console.log("Hello from external JS")
+
+// var :
+
+var captain = 45;  // declaration allowed
+
+captain = 'rohit'    // re-assignment is allowed
+
+var captain = 'hardik'  // re-declaration is allowed --> not recommended
+
+console.log(captain)
+
+// let :
+
+let rcb = 'rajat'   // declaration is allowed
+
+rcb = 'virat'       // re-assignment is allowed
+
+console.log(rcb)
+
+// let rcb ="faf"     // re-assignment is not allowed
+
+// const :
+
+const csk = 'dhoni'  // declaration is allowed
+
+// csk ='07'   re-assignment is allowed
+
+// re-declaration is not allowed
+
+
+// const gt;
+
+// gt = 'gill';     // not allowed , const must be initialize at declaration
